7a92f2fe1a14bb45c9f8b62c931c8bd65442cdb6
Adam Rodger <adam.rodger@gmail.com>
2023-12-01 14:11:00 +0000

    day(1): Packet "exclusive-cube" not found

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(error));
+        .route("/-1/error", get(error))
+        .route("/1/*nums", get(cube_many));
 
     Ok(router.into())
 }
@@ -15,4 +16,13 @@ async fn hello_world() -> &'static str {
 
 async fn error() -> StatusCode {
     StatusCode::INTERNAL_SERVER_ERROR
+}
+
+async fn cube_many(Path(nums): Path<String>) -> String {
+    nums.split('/')
+        .map(|n| n.parse::<i32>().unwrap())
+        .reduce(|acc, n| acc ^ n)
+        .unwrap()
+        .pow(3)
+        .to_string()
 }
